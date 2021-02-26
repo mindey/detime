9da209f42e07e2039365dbaa93107fdc2d75c91c
Mindey <mindey@qq.com>
2021-02-26 01:34:46 +0000

    add .gitignore, initial vesion

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r', encoding='utf-8') as f:
 
 setup(
     name='detime',
-    version='0.9.1',
+    version='1.0.0',
     description="Decimal Silicon Time: time since UNIX zero, in decimal.",
     long_description=long_description,
     long_description_content_type='text/rst',
